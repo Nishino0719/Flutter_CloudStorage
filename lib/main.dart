aa8d05707be0527f5a5e468b86893f12c355f2ed
Nishino0719 <nishino0719@icloud.com>
2020-09-18 23:54:03 +0900

    add test but do not run

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,241 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/widgets.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:uuid/uuid.dart';
+// import 'package:image_cropper/image_cropper.dart';
+// import 'package:image_picker/image_picker.dart';
+const String kTestString = 'Hello world!';
+
+void main() async{
+  // final FirebaseApp app = await myFirebaseApp();
+  final FirebaseOptions firebaseOptions = (Platform.isIOS || Platform.isMacOS)
+      ? const FirebaseOptions(
+    appId: '1:691469718157:ios:9bc8c1910c12da8632c8c4',
+    messagingSenderId: '691469718157',
+    apiKey: 'AIzaSyB58zXPkUsKAi-g82yViOBKMfiIDSeC4ow',
+    projectId: 'flutter-cloudstoragetest',
+  )
+      : const FirebaseOptions(
+    appId: '1:691469718157:ios:9bc8c1910c12da8632c8c4',
+    messagingSenderId: '691469718157',
+    apiKey: 'AIzaSyB58zXPkUsKAi-g82yViOBKMfiIDSeC4ow',
+    projectId: 'flutter-cloudstoragetest',
+  );
+  final FirebaseApp app =
+  await Firebase.initializeApp(name: 'flutter-cloudStorageTest', options: firebaseOptions);
+  final FirebaseStorage storage =
+  FirebaseStorage(app: app,storageBucket: 'gs://flutter-cloudstoragetest.appspot.com');
+
+  runApp(MyApp(storage: storage));
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  MyApp({this.storage});
+  final FirebaseStorage storage;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Cloud Storage',
+      home: MyHomePage(storage: storage),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyHomePage extends StatefulWidget{
+  MyHomePage({this.storage});
+  final FirebaseStorage storage;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  List<StorageUploadTask> _tasks = <StorageUploadTask>[];
 
-  void _incrementCounter() {
+  Future<void> _uploadFile() async {
+    final String uuid = Uuid().v1();
+    final Directory systemTempDir = Directory.systemTemp;
+    final File file = await File('${systemTempDir.path}/foo$uuid.txt').create();
+    await file.writeAsString(kTestString);
+    assert(await file.readAsString() == kTestString);
+    final StorageReference ref =
+    widget.storage.ref().child('text').child('foo$uuid.txt');
+    final StorageUploadTask uploadTask = ref.putFile(
+      file,
+      StorageMetadata(
+        contentLanguage: 'ja',
+        customMetadata: <String, String>{'activity': 'test'},
+      ),
+    );
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _tasks.add(uploadTask);
     });
+  }
+
+  Future<void> _downloadFile(StorageReference ref) async {
+    final String url = await ref.getDownloadURL();
+    final String uuid = Uuid().v1();
+    final http.Response downloadData = await http.get(url);
+    final Directory systemTempDir = Directory.systemTemp;
+    final File tempFile = File('${systemTempDir.path}/tmp$uuid.txt');
+    if (tempFile.existsSync()) {
+      await tempFile.delete();
+    }
+    await tempFile.create();
+    assert(await tempFile.readAsString() == "");
+    final StorageFileDownloadTask task = ref.writeToFile(tempFile);
+    final int byteCount = (await task.future).totalByteCount;
+    final String tempFileContents = await tempFile.readAsString();
+    assert(tempFileContents == kTestString);
+    assert(byteCount == kTestString.length);
+
+    final String fileContents = downloadData.body;
+    final String name = await ref.getName();
+    final String bucket = await ref.getBucket();
+    final String path = await ref.getPath();
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text(
+        'Success!\n Downloaded $name \n from url: $url @ bucket: $bucket\n '
+            'at path: $path \n\nFile contents: "$fileContents" \n'
+            'Wrote "$tempFileContents" to tmp.txt',
+        style: const TextStyle(color: Color.fromARGB(255, 0, 155, 0)),
+      ),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final List<Widget> children = <Widget>[];
+    _tasks.forEach((StorageUploadTask task) {
+      final Widget tile = UploadTaskListTile(
+        task: task,
+        onDismissed: () => setState(() => _tasks.remove(task)),
+        onDownload: () => _downloadFile(task.lastSnapshot.ref),
+      );
+      children.add(tile);
+    });
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Flutter Storage Example'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.clear_all),
+            onPressed:
+            _tasks.isNotEmpty ? () => setState(() => _tasks.clear()) : null,
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: children,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _uploadFile,
+        tooltip: 'Upload',
+        child: const Icon(Icons.file_upload),
+      ),
+    );
+  }
+}
+
+class UploadTaskListTile extends StatelessWidget {
+  const UploadTaskListTile(
+      {Key key, this.task, this.onDismissed, this.onDownload})
+      : super(key: key);
+
+  final StorageUploadTask task;
+  final VoidCallback onDismissed;
+  final VoidCallback onDownload;
+
+  String get status {
+    String result;
+    if (task.isComplete) {
+      if (task.isSuccessful) {
+        result = 'Complete';
+      } else if (task.isCanceled) {
+        result = 'Canceled';
+      } else {
+        result = 'Failed ERROR: ${task.lastSnapshot.error}';
+      }
+    } else if (task.isInProgress) {
+      result = 'Uploading';
+    } else if (task.isPaused) {
+      result = 'Paused';
+    }
+    return result;
+  }
+
+  String _bytesTransferred(StorageTaskSnapshot snapshot) {
+    return '${snapshot.bytesTransferred}/${snapshot.totalByteCount}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<StorageTaskEvent>(
+      stream: task.events,
+      builder: (BuildContext context,
+          AsyncSnapshot<StorageTaskEvent> asyncSnapshot) {
+        Widget subtitle;
+        if (asyncSnapshot.hasData) {
+          final StorageTaskEvent event = asyncSnapshot.data;
+          final StorageTaskSnapshot snapshot = event.snapshot;
+          subtitle = Text('$status: ${_bytesTransferred(snapshot)} bytes sent');
+        } else {
+          subtitle = const Text('Starting...');
+        }
+        return Dismissible(
+          key: Key(task.hashCode.toString()),
+          onDismissed: (_) => onDismissed(),
+          child: ListTile(
+            title: Text('Upload Task #${task.hashCode}'),
+            subtitle: subtitle,
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Offstage(
+                  offstage: !task.isInProgress,
+                  child: IconButton(
+                    icon: const Icon(Icons.pause),
+                    onPressed: () => task.pause(),
+                  ),
+                ),
+                Offstage(
+                  offstage: !task.isPaused,
+                  child: IconButton(
+                    icon: const Icon(Icons.file_upload),
+                    onPressed: () => task.resume(),
+                  ),
+                ),
+                Offstage(
+                  offstage: task.isComplete,
+                  child: IconButton(
+                    icon: const Icon(Icons.cancel),
+                    onPressed: () => task.cancel(),
+                  ),
+                ),
+                Offstage(
+                  offstage: !(task.isComplete && task.isSuccessful),
+                  child: IconButton(
+                    icon: const Icon(Icons.file_download),
+                    onPressed: onDownload,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
